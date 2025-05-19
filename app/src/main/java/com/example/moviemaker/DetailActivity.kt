6d18ee066efe04fe334c1c430d889f0e717c813f
mariatfrank <mtmfrank@gmail.com>
2025-05-19 20:43:22 -0300

    query inicial

--- app/src/main/java/com/example/moviemaker/DetailActivity.kt
+++ app/src/main/java/com/example/moviemaker/DetailActivity.kt
@@ -11,7 +11,7 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.queryInput)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/moviemaker/QueryActivity.kt
+++ app/src/main/java/com/example/moviemaker/QueryActivity.kt
@@ -1,20 +1,31 @@
 package com.example.moviemaker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class QueryActivity : AppCompatActivity() {
+    private lateinit var filmDao: FilmDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_query)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val input = findViewById<EditText>(R.id.queryInput)
+        val result = findViewById<TextView>(R.id.resultText)
+        val searchButton = findViewById<Button>(R.id.searchButton)
+
+        filmDao = AppDatabase.getInstance(this).filmDao()
+
+        searchButton.setOnClickListener {
+            val query = input.text.toString()
+            val results = filmDao.searchByName(query)
+            result.text = results.joinToString("\n") { it.name }
         }
     }
 }
