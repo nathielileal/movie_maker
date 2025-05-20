0aa491bbc151ba968ab8b4b37a74c9a95d40b1b4
PDRBS <pedroo.silva.33@gmail.com>
2025-05-19 22:11:55 -0300

    mainactivity + NewCharActivity

--- app/src/main/java/com/example/moviemaker/controller/MainActivity.kt
+++ app/src/main/java/com/example/moviemaker/controller/MainActivity.kt
@@ -7,13 +7,16 @@ import com.example.moviemaker.R
 import com.example.moviemaker.model.Filme
 import android.widget.ListView
 import android.widget.TextView
-import com.example.moviemaker.data.FilmDao
 import kotlinx.coroutines.ParentJob
 import android.content.Intent
+import android.view.View
+import android.widget.ArrayAdapter
+import com. example. moviemaker. DetailActivity
+import com.example.moviemaker.data.FilmeDAO
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var filmDao : FilmDao
+    private lateinit var filmDao : FilmeDAO
     private lateinit var listView: ListView
     private lateinit var emptyTextView : TextView
 
@@ -21,24 +24,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
 
         listView = findViewById(R.id.lvChars)
         emptyTextView = findViewById(R.id.tvEmpty)
-        filmDao = FilmDao(this)
+        filmDao = FilmeDAO(this)
         listAllChars()
 
         listView.onItemClickListener { parent, view, position, id ->
             val selectedChar = parent.getItemAtPosition(position) as Filme
-            val intent = Intent(this, DetailActivity::class.java)
-
+            val intent = Intent(this, DetailActivity::class.java).apply {
+                putExtra("id_filme", selectedChar.id_filme)
+            }
+            startActivity(intent)
         }
-=======
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.queryInput)) { v, insets ->
-       //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-       //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      //      insets
-     //   }
->>>>>>> 947e4f1cee256f5a8fffba25a083f6b5ec597420
+
+    }
+
+    private fun listAllChars() {
+
+        val chars = filmDao.get()
+        if(chars.isEmpty()) {
+            listView.visibility = ListView.GONE
+            emptyTextView.visibility = TextView.VISIBLE
+        } else {
+            listView.visibility = ListView.VISIBLE
+            emptyTextView.visibility = TextView.GONE
+            val adapter : ArrayAdapter<Filme> = ArrayAdapter(this, android.R.layout.simple_list_item_1, chars)
+            listView.adapter = adapter
+        }
+    }
+
+    fun newChar(view:View) {
+        val intent = Intent(this, NewCharActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listAllChars()
     }
 }
