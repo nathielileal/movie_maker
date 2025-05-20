a7217f401006275d51bcfe0aca9f30a73f0ae54d
nathieli <nathielileal123@gmail.com>
2025-05-19 22:42:09 -0300

    feat: insercao de dois filmes no inicio

--- app/src/main/java/com/example/moviemaker/data/db/DBHelper.kt
+++ app/src/main/java/com/example/moviemaker/data/db/DBHelper.kt
@@ -4,7 +4,8 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,DATABASE_VERSION) {
+class DBHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     companion object {
         const val DATABASE_NAME = "movie_maker.db"
@@ -14,6 +15,7 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,
 
     override fun onCreate(db: SQLiteDatabase) {
         val createTable = """
+            
         CREATE TABLE $TABLE_NAME (
             id_filme INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
@@ -22,7 +24,31 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,
             sinopse TEXT
         )
     """.trimIndent()
+
         db.execSQL(createTable)
+
+        val insertFilme1 = """
+        INSERT INTO $TABLE_NAME (nome, data_lancamento, genero, sinopse)
+        VALUES ('Como treinar o seu dragão', 
+        '2010-03-26', 
+        'Ação', 
+        'Soluço é um jovem viking que não tem capacidade para lutar contra os dragões, como é a tradição local. 
+        Sua vida muda quando ele ajuda um dragão que lhe mostra toda a verdade. Juntos, eles tentam provar que 
+        dragões e humanos podem ser amigos.')
+    """.trimIndent()
+
+        val insertFilme2 = """
+        INSERT INTO $TABLE_NAME (nome, data_lancamento, genero, sinopse)
+        VALUES ('Como treinar o seu dragão', 
+        '2014-06-19', 
+        'Ação', 'Cinco anos se passaram desde que Soluço estabeleceu a paz com os dragões e vive em harmonia,
+         na Ilha de Berk, com Banguela. Eles voam, apostam corridas e se divertem muito. Em uma destas aventuras, 
+descobrem uma caverna secreta cheia de dragões. Agora, a dupla luta para proteger Berk de um guerreiro perigoso, 
+chamado Drago Bludvist, que deseja controlar todos os dragões existentes.')
+    """.trimIndent()
+
+        db.execSQL(insertFilme1)
+        db.execSQL(insertFilme2)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
