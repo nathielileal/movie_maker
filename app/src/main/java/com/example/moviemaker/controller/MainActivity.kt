288fac5787c98c1246676d02abc12c9741085d69
PDRBS <pedroo.silva.33@gmail.com>
2025-05-19 21:46:18 -0300

    feat/mainactivity upgrade

--- app/src/main/java/com/example/moviemaker/controller/MainActivity.kt
+++ app/src/main/java/com/example/moviemaker/controller/MainActivity.kt
@@ -5,23 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moviemaker.R
 import com.example.moviemaker.model.Filme
+import android.widget.ListView
+import android.widget.TextView
+import com.example.moviemaker.data.FilmDao
+import kotlinx.coroutines.ParentJob
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
-    private fun createFilms() : List<Filme>{
-        return listOf(
-           // Filme(1, "Filme1", 1999, "Terror", "lalala"),
-           // Filme(2, "Filme2", 1998, "Ação", "lololo")
-        )
-    }
+    private lateinit var filmDao : FilmDao
+    private lateinit var listView: ListView
+    private lateinit var emptyTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.queryInput)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        listView = findViewById(R.id.lvChars)
+        emptyTextView = findViewById(R.id.tvEmpty)
+        filmDao = FilmDao(this)
+        listAllChars()
+
+        listView.onItemClickListener { parent, view, position, id ->
+            val selectedChar = parent.getItemAtPosition(position) as Filme
+            val intent = Intent(this, DetailActivity::class.java)
+
         }
     }
 }
