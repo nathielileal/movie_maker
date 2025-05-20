4074accc0a6d89a4e2c36d3b3da2bbea2ce69015
PDRBS <pedroo.silva.33@gmail.com>
2025-05-19 22:26:53 -0300

    continuidade do newchar

--- app/src/main/java/com/example/moviemaker/controller/NewCharActivity.kt
+++ app/src/main/java/com/example/moviemaker/controller/NewCharActivity.kt
@@ -1,13 +1,26 @@
 package com.example.moviemaker.controller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.moviemaker.R
+import com.example.moviemaker.data.FilmeDAO
+import com.example.moviemaker.model.Filme
 
 class NewCharActivity : AppCompatActivity() {
+
+    private lateinit var FilmeDao : FilmeDAO
+    private var id_filme : Int = 0
+    private lateinit var etName : EditText
+    private lateinit var etYear : EditText
+    private lateinit var etGenre : EditText
+    private lateinit var etSinopse : EditText
+    private lateinit var btnSave : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
